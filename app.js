0e9f4b6778e0b9141885fcd36b4864ed2e387367
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-02-19 00:40:31 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,171 @@
+var fs = require('fs'),
+    express = require('express'),
+    app = express.createServer(),
+    restler = require('restler'),
+    util = require('util');
+
+var redis = require("redis"),
+    client = redis.createClient();
+
+app.use(express.bodyParser());
+app.listen(process.env.PORT || 9000);
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+console.log("Initializing devoxx cache application");
+
+function removeParameters(url, parameters) {
+
+  for (var id = 0 ; id < parameters.length ; id++) {
+      var urlparts= url.split('?');
+
+      if (urlparts.length>=2)
+      {
+          var urlBase=urlparts.shift(); //get first part, and remove from array
+          var queryString=urlparts.join("?"); //join it back up
+
+          var prefix = encodeURIComponent(parameters[id])+'=';
+          var pars = queryString.split(/[&;]/g);
+          for (var i= pars.length; i-->0;)               //reverse iteration as may be destructive
+              if (pars[i].lastIndexOf(prefix, 0)!==-1)   //idiom for string.startsWith
+                  pars.splice(i, 1);
+          var result = pars.join('&');
+          url = urlBase + (result ? '?' + result : '');
+      }
+  }
+
+  return url;
+}
+
+//function removeVariableFromURL(url_string, variable_name) {
+//    var URL = String(url_string);
+//    var regex = new RegExp( "\\?" + variable_name + "=[^&]*&?", "gi");
+//    URL = URL.replace(regex,'?');
+//    regex = new RegExp( "\\&" + variable_name + "=[^&]*&?", "gi");
+//    URL = URL.replace(regex,'&');
+//    URL = URL.replace(/(\?|&)$/,'');
+//    regex = null;
+//    return URL;
+//  }
+
+
+function getParameterByName( url, name ) {
+    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regex = new RegExp( "[\\?&]" + name + "=([^&#]*)" );
+    var results = regex.exec( url );
+    if( results == null ) {
+        return "";
+    }
+    else {
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+}
+
+function sendJsonResponse(options, data) {
+
+    var callback = getParameterByName(options.req.url, 'callback');
+
+    var response = data;
+    if (callback) {
+        options.res.header('Content-Type', 'application/javascript');
+        response = callback + '(' + response + ');';
+    }
+    else {
+        options.res.header('Content-Type', 'application/json');
+    }
+
+    console.log("[" + options.url + "] Response sent: " + response);
+    options.res.send(response);
+}
+
+app.all('/*', function(req, res) {
+    try {
+
+        if (req.method !== 'GET') {
+            var errorMessage = "Request method not supported: '" + req.method + "'";
+            console.log(errorMessage);
+            res.send(errorMessage, 501);
+            return;
+        }
+
+        var cacheKey = removeParameters(req.url, ['callback', '_']);
+        var urlToFetch = removeParameters(req.url, ['callback']);
+
+        console.log("[" + cacheKey + "] Cache Key: " + cacheKey);
+        console.log("[" + cacheKey + "] Checking if data is in cache");
+
+        var clearCache = getParameterByName(req.url, 'clear') === 'true';
+        var forceNoCache = getParameterByName(req.url, 'cache') === 'false';
+        if (forceNoCache) {
+            var options = {
+                req: req,
+                res: res,
+                url:urlToFetch,
+                cacheKey: cacheKey,
+                forceNoCache: forceNoCache,
+                clearCache: clearCache
+            };
+            processRequest(options);
+        }
+        else {
+            client.get(cacheKey, function (err, data) {
+                var options = {
+                    req: req,
+                    res: res,
+                    cachedData: data,
+                    err: err,
+                    url:urlToFetch,
+                    cacheKey: cacheKey,
+                    forceNoCache: forceNoCache,
+                    clearCache: clearCache
+                };
+                processRequest(options);
+            });
+        }
+    } catch(err) { res.send(e.name + ": " + e.message, 500); }
+});
+
+function processRequest(options) {
+    try {
+        if (!options.forceNoCache && options.clearCache) {
+            console.log("[" + options.url + "] Clearing cache for key: '" + options.cacheKey + "'");
+             client.del(options.cacheKey);
+         }
+
+        if (!options.err && options.cachedData) {
+            console.log("[" + options.url + "] A reply is in cache key: '" + options.cacheKey + "', returning immediatly the reply");
+            sendJsonResponse(options, options.cachedData);
+        }
+        else {
+            console.log("[" + options.url + "] No cached reply found for key: '" + options.cacheKey + "'");
+            var targetUrl = 'https://cfp.devoxx.com' + options.cacheKey;
+            console.log("[" + options.url + "] Fetching data from url: '" + targetUrl + "'");
+            restler.get(targetUrl).on('complete', function (data, response) {
+                var contentType = response.header("Content-Type");
+                console.log("[" + options.url + "] Http Response - Content-Type: " + contentType);
+                if ( contentType.indexOf('json') === -1 &&
+                     contentType.indexOf('script') === -1 ) {
+
+                    console.log("[" + options.url + "] Content-Type is not json or javascript: Not caching data and returning response directly");
+                    options.res.header('Content-Type', contentType);
+                    options.res.send(data);
+                }
+                else {
+                    var jsonData =  JSON.stringify(data);
+                    console.log("[" + options.url + "] Fetched Response from url '" + targetUrl + "': " + jsonData);
+                    sendJsonResponse(options, jsonData);
+                    if (!options.forceNoCache) {
+                        client.set(options.cacheKey, jsonData);
+                        client.expire(options.cacheKey, 60 * 60);
+                    }
+                }
+            });
+        }
+    } catch(err) {
+        var errorMessage = e.name + ": " + e.message;
+        console.log(errorMessage);
+        options.res.send(errorMessage, 500);
+    }
+}
